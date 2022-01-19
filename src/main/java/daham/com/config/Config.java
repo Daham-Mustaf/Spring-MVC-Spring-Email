0759b1e56fb30042f1a1dd9366c3f27789fc117b
Daham-Mustaf <s7damust@uni-bonn.de>
2022-01-19 10:54:04 +0100

    commit app

---
+++ src/main/java/daham/com/config/Config.java
@@ -0,0 +1,22 @@
+package daham.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.nio.charset.StandardCharsets;
+
+public class EmailConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver htmlTemplateResolver(){
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix("/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
+}
+
---
+++ src/main/java/daham/com/config/WebInitializer.java
@@ -0,0 +1,29 @@
+package daham.com.config;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+
+//		Class<?>[] configClasses  = {AppConfig.class};
+//		return configClasses;
+
+        return new Class<?>[] {EmailConfig.class} ;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        // String[] mapping = { "/" };
+        // getServletMappings(): This declares the servlet root URI.
+
+        return new String[] { "/" };
+    }
+
+}
